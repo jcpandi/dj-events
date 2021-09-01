6b5c8f62acd13f859a2ed353e4551d2ee23784be
JC Pandi <jcpandi@gmail.com>
2021-09-01 22:19:19 +0800

    inital

---
+++ pages/events/[slug].js
@@ -0,0 +1,82 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
+
+export default function EventPage({ evt }) {
+  const deleteEvent = (e) => {
+    console.log('delete')
+  }
+
+  return (
+    <Layout>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt />Edit
+            </a>
+          </Link>
+          <a href='#' className={styles.delte}
+          onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+     
+     </div>
+     </div>
+     <span>
+       {evt.name} at {evt.time}
+     </span>
+     <h1>{evt.name}</h1>
+     {evt.image && (
+       <div className={styles.image}>
+         <Image src={evt.image} width={960} height={600} />
+
+       </div>
+     )}
+     <h3>Peformers:</h3>{evt.performers}
+     <h3>Description:</h3>{evt.description}
+     <h3>Venue: </h3>{evt.venue}
+     <p>{evt.address}</p>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }))
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  }
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   }
+// }
